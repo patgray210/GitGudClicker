5453582be7c4661aa436b201c6c4d4d20e18783e
Patrick Gray <patgray210@gmail.com>
2020-05-14 13:41:08 -0500

    final

--- scripts/list-view.js
+++ scripts/list-view.js
@@ -1,20 +1,25 @@
 let listAmount=0;
+var data;
 const initViews = function(){
     getFromGoogleDB();
 }
 
 const showData = function(sheetData){
-    
-    
     clearChat();
     listAmount=0;
-        for (let row of sheetData.values){
+    data = sheetData.values;
+    console.log(data);
+    data.sort(compareScore)
+        for (let row of data){
+            console.log(row);
             addData(row);
             listAmount++;
         }
-    
+    }
+const compareScore =function(a,b){
+    console.log(b[2], a[2]);
+   return b[2]- a[2];
 }
-
 
 const addData = function(row){
     if(listAmount<11){
